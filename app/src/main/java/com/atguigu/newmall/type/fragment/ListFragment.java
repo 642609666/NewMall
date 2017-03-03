ae2c57c7c8d990afbc256bfc8cd5fc815bda1e79
642609666 <642609666@qq.com>
2017-03-03 16:35:49 +0800

    使用fastjson解析数据

--- app/src/main/java/com/atguigu/newmall/type/fragment/ListFragment.java
+++ app/src/main/java/com/atguigu/newmall/type/fragment/ListFragment.java
@@ -6,12 +6,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.newmall.R;
 import com.atguigu.newmall.base.BaseFragment;
 import com.atguigu.newmall.type.adapter.TypeLeftAdapter;
+import com.atguigu.newmall.type.bean.TypeBean;
+import com.atguigu.newmall.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
 
 /**
  * Created by ${
@@ -30,6 +38,11 @@ public class ListFragment extends BaseFragment {
     //网络请求得到数据
     private String[] titles = new String[]{"小裙子", "上衣", "下装", "外套", "配件", "包包", "装扮", "居家宅品",
             "办公文具", "数码周边", "游戏专区"};
+    //联网的url的集合
+    private String[] urls = new String[]{Constants.SKIRT_URL, Constants.JACKET_URL, Constants.PANTS_URL, Constants.OVERCOAT_URL,
+            Constants.ACCESSORY_URL, Constants.BAG_URL, Constants.DRESS_UP_URL, Constants.HOME_PRODUCTS_URL, Constants.STATIONERY_URL,
+            Constants.DIGIT_URL, Constants.GAME_URL};
+    private List<TypeBean.ResultEntity> mResult;
 
     @Override
     public View initView() {
@@ -53,10 +66,41 @@ public class ListFragment extends BaseFragment {
 //                Toast.makeText(mContext, "" + position, Toast.LENGTH_SHORT).show();
                 //传入被点击的位置
                 mLeftAdapter.changeSelected(position);
+
                 //刷新适配器
                 mLeftAdapter.notifyDataSetChanged();
             }
         });
-
+        //联网请求
+        getDataFromNet(urls[0]);
     }
+
+    private void getDataFromNet(final String url) {
+        OkHttpUtils.get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG", "list分页请求失败" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("TAG", "list分页请求成功");
+                        processData(response);
+                    }
+
+
+                });
+        //TypeBean typeBean = JSON.parseObject(url, TypeBean.class);
+        // List<TypeBean.ResultEntity> result = typeBean.getResult();
+    }
+
+    private void processData(String url) {
+        TypeBean typeBean = JSON.parseObject(url, TypeBean.class);
+        mResult = typeBean.getResult();
+        Log.e("TAG", "点击的" + mResult.get(0).getName());
+    }
+
 }
