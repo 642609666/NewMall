a555851dacaab86f45f41a598a25d92126ec0a95
642609666 <642609666@qq.com>
2017-03-04 16:55:38 +0800

    联网请求网络且解析数据

--- app/src/main/java/com/atguigu/newmall/community/fragment/NewPostFragment.java
+++ app/src/main/java/com/atguigu/newmall/community/fragment/NewPostFragment.java
@@ -1,12 +1,23 @@
 package com.atguigu.newmall.community.fragment;
 
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.newmall.R;
 import com.atguigu.newmall.base.BaseFragment;
+import com.atguigu.newmall.community.bean.NewPostBean;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+
+import static com.atguigu.newmall.utils.Constants.NEW_POST_URL;
 
 /**
  * Created by ${
@@ -16,20 +27,21 @@ import com.atguigu.newmall.base.BaseFragment;
  */
 
 public class NewPostFragment extends BaseFragment {
-    private TextView mTextView;
+
+    @BindView(R.id.lv_new_post)
+    ListView lvNewPost;
+    private List<NewPostBean.ResultEntity> mResult;
 
     /**
-     * 初始化个人视图
+     * 初始新帖视图
      *
      * @return
      */
     @Override
     public View initView() {
-        mTextView = new TextView(mContext);
-        mTextView.setTextColor(Color.RED);
-        mTextView.setTextSize(30);
-        mTextView.setGravity(Gravity.CENTER);
-        return mTextView;
+        View view = View.inflate(mContext, R.layout.fragment_new, null);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     /**
@@ -37,7 +49,35 @@ public class NewPostFragment extends BaseFragment {
      */
     @Override
     protected void initData() {
-        Log.e("TAG", "UserFragment initData()++新的数据加载");
-        mTextView.setText("新的数据");
+        Log.e("TAG", "UserFragment initData()++新帖加载");
+
+        //联网获取数据
+        getDataFromNet();
+    }
+
+    public void getDataFromNet() {
+        OkHttpUtils.get()
+                .url(NEW_POST_URL)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG", "新帖页面解析失败" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("TAG", "新帖页面解析成功" + response);
+
+                        //解析数据
+                        disposeData(response);
+                    }
+                });
+
+    }
+
+    private void disposeData(String json) {
+        NewPostBean newPostBean = JSON.parseObject(json, NewPostBean.class);
+        mResult = newPostBean.getResult();
     }
 }
